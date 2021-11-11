f0485bfcb62d0b308e5d398108c4fbddb71b9fc6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-11 22:21:30 +0200

    Initial Commit

---
+++ BuildingSwiftUIXcodePreviewUsingJSONFile/BuildingSwiftUIXcodePreviewUsingJSONFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingSwiftUIXcodePreviewUsingJSONFileApp.swift
+//  BuildingSwiftUIXcodePreviewUsingJSONFile
+//
+//  Created by ramil on 11.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BuildingSwiftUIXcodePreviewUsingJSONFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
